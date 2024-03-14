7de7ba312a496f8ce799ef6429bcc6e06c39e59b
Mikolaj <mikuspl0900@gmail.com>
2024-03-14 21:13:03 +0100

    opis tego

---
+++ komendy.sh
@@ -0,0 +1,64 @@
+git commit
+git branch my branch
+git checkout my branch; git commit
+git checkout
+git checkout -b <nazwa-gałęzi>
+git checkout main
+git merge bugFix
+git checkout (sth); git merge (sth)
+git checkout bugFix^
+git rebase main
+git checkout one/two
+git branch -f three C2
+git cherry-pick C5 C4 C3 C2
+git cherry-pick C4 C3 C2
+git branch ^
+git branch -f main C6
+git reset
+git reverb
+git cherry-pick C3 C4
+git rebase -i overHere --solution-ordering C3,C4,C4
+git rebase -i main --solution-ordering C4
+git rebase bugFix main
+git rebase -i HEAD~2 --solution-ordering C3,C2
+git rebase caption main
+git rebase main bugFix
+git rebase bugFix side
+git rebase side another
+git rebase another main
+git branch bugWork main^^2^
+git cherry-pick C2
+git cherry-pick C3
+git rebase -i
+git commit --amend
+git describe <ref>
+git checkout HEAD~1
+git branch -f bugFix
+reset
+git reset HEAD~1
+git checkout pushed
+git revert HEAD
+tag
+git describe
+git clone
+git checkout o/main
+git fetch
+git fakeTeamwork 2
+git fakeTeamwork
+git checkout origin feathure
+git push origin feathure
+git rebase o/main side1
+git rebase side1 side2
+git rebase side2 side3
+git rebase side3 main
+git merge side1/2/3
+git pull --rebase
+~ ^
+git fetch
+git merge o/main
+git push 
+git pull
+git push origin main
+git push origin foo
+git fetch origin :bar
+git push origin :foo
